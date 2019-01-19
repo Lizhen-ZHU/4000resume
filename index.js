35a2ee5f636f1a211704d5826362ea7c07cac0d4
Lizhen-ZHU <43251902+Lizhen-ZHU@users.noreply.github.com>
2019-01-19 17:26:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,45 @@ portfolio2.onclick=function(){
 portfolio3.onclick=function(){
   portfolioBar.className='bar state-3'
 }
+// ------------------nav bar----------------
+
+window.onscroll = function(scrolled){
+  if (window.scrollY > 20){
+    topnavbar.classList.add('sticky')
+  }else{
+      topnavbar.classList.remove('sticky')
+  }
+}
+
+
+let liTags = document.querySelectorAll('nav.topnavbar > ul > li')
+for(let i=0; i<liTags.length; i++){
+  liTags[i].onmouseenter = function(x){
+  x.currentTarget.classList.add('active')
+
+  }
+  liTags[i].onmouseleave = function(x){
+    x.currentTarget.classList.remove('active')
+  }
+}
+// ------------------nav bar----------------
+
+// -------------------scoll to ---------------
+
+
+let aTags = document.querySelectorAll('nav.topnavbar > ul > li > a')
+for(let i=0; i<aTags.length; i++){
+aTags[i].onclick = function(x){
+  x.preventDefault();
+  let a = x.currentTarget
+  let href = a.getAttribute('href') //#siteAbout
+  let element = document.querySelector(href)
+  let top = element.offsetTop //距离顶部距离
+  window.scrollTo(0, top - 80)
+}
+
+}
+
+
+
+// -------------------scoll to ---------------
